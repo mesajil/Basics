af1aa3f7d5d48506915c829af28823dc092fdc50
Luis Mesajil <lhmesajil@gmail.com>
2020-09-27 16:11:03 -0500

    M clase1.js Sentencia if

--- Js/clase1.js
+++ Js/clase1.js
@@ -1,25 +1,23 @@
+let MAYOR_EDAD = 18;
 
+persona = {
+    name: "Juan",
+    isStudent: true,
+    age: 23
+}
 
 
 function myFun ({name, age} = {name: "Luis", age: 23}){
     console.log(`Hola me llamo ${name} y tengo ${age} años`);
 }
 
-function createNewOb (ob){
-    return {
-        ...ob,
-        age: ob.age + 1,
-        sueldo: 2000
-    };
+function createNewOb ({ ...ob}){
+    ob.age = 10
+    console.log(ob.age);
 }
-
-ob1 = {
-    name: "Luis"
+createNewOb(persona);
+if (persona.isStudent){
+    console.log(`${persona.name} is student`);
 }
-
-ob2 = {
-    ...ob1
-}
-ob1.name = "Juan"
-console.log(ob1)
-console.log(ob2)
+if (persona.age >= MAYOR_EDAD) console.log("Es mayor de edad");
+else console.log("No es mayor de edad");
