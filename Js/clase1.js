4d901da7ae91c254fdc41d94ced730547db61e5f
Luis Mesajil <lhmesajil@gmail.com>
2020-09-28 13:39:52 -0500

    M clase1.js

--- Js/clase1.js
+++ Js/clase1.js
@@ -1,9 +1,9 @@
-let MAYOR_EDAD = 18;
+const MAYOR_EDAD = 18;
 
 persona = {
     name: "Juan",
     isStudent: true,
-    age: 23
+    age: 18
 }
 
 
@@ -15,9 +15,14 @@ function createNewOb ({ ...ob}){
     ob.age = 10
     console.log(ob.age);
 }
+
+function esMayor(x,y){
+    return x >= y;
+}
+
 createNewOb(persona);
 if (persona.isStudent){
     console.log(`${persona.name} is student`);
 }
-if (persona.age >= MAYOR_EDAD) console.log("Es mayor de edad");
+if (esMayor(persona.age, MAYOR_EDAD)) console.log("Es mayor de edad");
 else console.log("No es mayor de edad");
