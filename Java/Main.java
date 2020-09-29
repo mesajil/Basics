60446d6c520e1017afb28c0570e022c82b56285c
Luis Mesajil <lhmesajil@gmail.com>
2020-09-29 12:42:45 -0500

    A Patient

--- Java/Main.java
+++ Java/Main.java
@@ -2,8 +2,7 @@ import ui.Menu;
 public class Main {
     public static void main (String[] args){
         Doctor myDoctor = new Doctor("72397609", "Luis Mesajil", "Developer");
-
+        Patient myPatient = new Patient ("Roxana", "roxana@gmail.com");
         Menu.show ();
-        System.out.println (Menu.MESES);        
     }
 }
---
+++ Java/Patient.java
@@ -0,0 +1,14 @@
+
+
+public class Patient {
+    String name;
+    String email;
+    String address;
+    String phoneNumber;
+    String blood;
+
+    Patient (String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+}
