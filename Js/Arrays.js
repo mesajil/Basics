f543b87e6023bc2d90376df898c4d4248e2d928c
Luis Mesajil <lhmesajil@gmail.com>
2020-10-04 15:40:03 -0500

    A Js/Arrays.js
    Manejo e impresión de objetos utilizando arrreglos

---
+++ Js/Arrays.js
@@ -0,0 +1,34 @@
+var per1 = {
+    nombre: 'Sacha',
+    apellido: 'Lifszyc',
+    altura: 1.72
+}
+
+var per2 = {
+    nombre: 'Alan',
+    apellido: 'Perez',
+    altura: 1.86
+}
+
+var per3 = {
+    nombre: 'Martin',
+    apellido: 'Gomez',
+    altura: 1.85
+}
+
+
+
+let personas = [per1, per2, per3]
+
+for (let p of personas){
+    
+    console.log (p["apellido"])
+}
+
+
+
+
+
+
+
+
