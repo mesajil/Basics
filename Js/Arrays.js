b7af40fd5cc2a5e0da264403c544d07ab765c1bf
Luis Mesajil <lhmesajil@gmail.com>
2020-10-05 10:42:24 -0500

    M Js/Arrays: Utilizacion de metodos del array
    Métodos del array utilizados: filter, map, find

--- Js/Arrays.js
+++ Js/Arrays.js
@@ -15,20 +15,53 @@ var per3 = {
     apellido: 'Gomez',
     altura: 1.85
 }
-
-
-
 let personas = [per1, per2, per3]
 
-for (let p of personas){
-    
-    console.log (p["apellido"])
+// Filtrar utilizando for of
+// let filter = []
+// for (let per of personas){
+//     if (per.altura <= 1.85){
+//         filter.push (per)
+//     }
+// }
+
+// // Filtrar con el método filter
+
+// let filter = personas.filter (
+//     (per) => per.altura > 1.85
+// )
+
+
+// // Imprimir filtro
+// for (let per of filter){
+//     console.log (`${per.nombre} tiene una altura de ${per.altura}`)
+// }
+
+
+// // Crear array de apellidos utilizando el método map
+
+// let apellidos = personas.map(
+//     (per) => per.apellido
+// )
+// console.log(personas)
+// // Imprimir apellidos
+// for (apellido of apellidos){
+//     console.log(apellido)
+// }
+
+
+// // Filtrar persona utilizando el método find
+
+// let per = personas.some (p => p.nombre == "Sacha")
+// console.log (per)
+
+// Modificar altura de las personas a centímetros utilizando el método map
+
+let cambiarAltura = (per) => ({...per, altura: per.altura * 100})
+let personasCms = personas.map (cambiarAltura)
+console.table(personasCms)
+for (let per of personasCms){
+    console.table(per)
 }
-
-
-
-
-
-
 
 
